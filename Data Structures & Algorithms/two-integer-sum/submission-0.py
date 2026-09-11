8460ba339db2ab79f046ad855000abf18fa923ee
Ethan Hans Harianto <79932428+ethanharianto@users.noreply.github.com>
2026-09-11 15:40:37 -0400

    Update: two-integer-sum - submission-0

--- Data Structures & Algorithms/two-integer-sum/submission-0.py
+++ Data Structures & Algorithms/two-integer-sum/submission-0.py
@@ -1,8 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        stash = {}
-        for i in range(len(nums)):
-            if (target - nums[i]) in stash:
-                return [stash[target - nums[i]], i]
-            else:
-                stash[nums[i]] = i
+        addends = {}
+        for i, num in enumerate(nums):
+            addend = target - num
+            if addend in addends:
+                return [addends[addend], i]
+            addends[num] = i
+        
